793b06493e8db01b018114a8f201f96d7ea49c71
jared salazar loera <salazarloerajared@gmail.com>
2023-10-05 23:41:19 -0600

    <FEAT>: animacion del botton para dia y noche

--- app/src/main/java/com/example/lucescasa/MainActivity.java
+++ app/src/main/java/com/example/lucescasa/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import android.animation.ObjectAnimator;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,8 +22,20 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView dayNightImageView;
+    private Button toggleButton;
+    private boolean isDay = true;
     private RequestQueue rQueue;
     private Button btn;
     String adafruitURL = "https://io.adafruit.com/api/v2/JaredLoera/feeds/iluminacion/data";
@@ -28,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
     String estadoFoco;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+
+        dayNightImageView = findViewById(R.id.dayNightImageView);
+        toggleButton = findViewById(R.id.toggleButton);
         rQueue = Volley.newRequestQueue(this);
+        getLastDataFeedIluminacion();
+        /*
         btn = findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,18 +59,86 @@ public class MainActivity extends AppCompatActivity {
                 requestQue(status,btn);
             }
         });
+        */
+    }
 
+    public void getLastDataFeedIluminacion(){
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, "https://io.adafruit.com/api/v2/JaredLoera/feeds/iluminacion", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String  value = response.getString("last_value");
+                    int lastData= Integer.parseInt(value);
+                    if(lastData == 1){
+                        status = true;
+                        toggleButton.setText("Encendido");
+                    }else{
+                        status = false;
+                        toggleButton.setText("Apagado");
+                        dayNightImageView.setImageResource(R.drawable.moon);
+                        isDay = false;
+                    }
+                }catch (JSONException error){
+                    error.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+               error.printStackTrace();
+            }
+        }){
+            @Override
+            public Map<String,String> getHeaders(){
+                Map<String,String> headers = new HashMap<>();
+                headers.put("X-AIO-Key","aio_OmFm05QNZnpqTH5DjLVLOwC1LoMz");
+                return headers;
+            }
+        };
+        rQueue.add(request);
+    }
+    public void  toggleDayNight (View view){
+        float currentAlpha = dayNightImageView.getAlpha();
+        float targetAlpha = (currentAlpha == 1) ? 0 : 1;
+
+        Animation animation = new AlphaAnimation(currentAlpha, targetAlpha);
+        animation.setDuration(2000);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                requestQue(status);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (isDay){
+                    dayNightImageView.setImageResource(R.drawable.moon);
+                    isDay =false;
+                    toggleButton.setText("Apagado");
+                }else {
+                    dayNightImageView.setImageResource(R.drawable.sun);
+                    isDay = true;
+                    toggleButton.setText("Encendido");
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        dayNightImageView.startAnimation(animation);
 
     }
-    public  void requestQue(boolean estado,Button btn){
+
+    public  void requestQue(boolean estado){
         if(estado){
             estadoFoco ="0";
             status = false;
-            btn.setText("Apagado");
         }else{
             estadoFoco ="1";
             status = true;
-           btn.setText("Encendido");
         }
         JSONObject objeto = new JSONObject();
         try {
@@ -74,9 +161,8 @@ public class MainActivity extends AppCompatActivity {
         }) {
             @Override
             public Map<String, String> getHeaders() {
-
                 Map<String, String> headers = new HashMap<>();
-                headers.put("X-AIO-Key", "aio_PzLI65y4r47JYAOL0xxSTlubHtog");
+                headers.put("X-AIO-Key", "aio_OmFm05QNZnpqTH5DjLVLOwC1LoMz");
                 return headers;
             }
         };
